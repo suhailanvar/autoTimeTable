528f43797a3707554acce790ca7f8ba211c02daa
suhail-cs23-duk <suhail.cs23@duk.ac.in>
2024-08-22 12:01:09 +0530

    Added a check to see if there are default timetable records for a particular degree. If not, then it creates one, with default subject value as 0.

--- routes/timetable.py
+++ routes/timetable.py
@@ -4,12 +4,79 @@ def register_timetable(app, rt, db):
     courses = db.t.course
     timetable = db.t.timetable
     Timetable = timetable.dataclass()
+    degree = db.t.degree
+
+
+
+    # creates default db values for each degree if it does not exist
+    def create_timetable_db_template() :
+        degrees = [x["id"] for x in degree()]
+        degrees_with_timetables = [x.degree_id for x in timetable(where="year='2024' and sem='3'", select="distinct degree_id")]
+
+        for d in degrees :
+            if d not in degrees_with_timetables :
+                # insert default timetable values for each degree based on the first degree
+                # should update this to be based on a base template
+                for t in timetable(where="year='2024' and sem='3' and degree_id=1") :
+                    timetable.insert(year="2024", sem="3", degree_id=d, time=t.time)  # day values should default to 0
+
+        print("Hello!",degrees_with_timetables)
+
+    create_timetable_db_template()
+
     @rt('/view/timetable/')
-    def get(year:str="2024", sem:str="3", degree:str="Msc MI"):
+    def get(session, year:str=None, sem:str=None, deg:str=None):
+
+        print("Inside Get sess",session)
+        print("Inside Get2",year, sem, deg)
+
+        if not session['year'] :
+            if year : session['year'] = year
+            else : session['year'] = "2024"
+        else : year = session['year']
+
+        if not session['sem'] :
+            if sem : session['sem'] = sem
+            else : session['sem'] = "3"
+        else : sem = session['sem']
+
+        if not session['degree'] :
+            if deg : session['degree'] = deg
+            else : session['degree'] = 1
+        else : deg = session['degree']
+
+        print("Inside Get3 sess", year, sem, deg)
+        print("Inside Get4", year, sem, deg)
+
+        # print(session)
+
+        def get_year_dropdown(year):
+            yearlist = [x.year for x in timetable(select="distinct year")]
+            # print(yearlist)
+            return Select(
+                *[Option(x, value=x, selected=(True if x == year else False)) for x in yearlist],
+                name="year", id="year",
+            )
+
+        def get_sem_dropdown(sem):
+            semlist = [x.sem for x in timetable(select="distinct sem")]
+            # print(semlist)
+            return Form(Select(
+                *[Option(f"Semester {x}", value=x, selected=(True if x == sem else False)) for x in semlist],
+                name="sem", id="sem"
+            ))
+
+        def get_degree_dropdown(deg):
+            degreelist = [x for x in degree()]
+            # print(degreelist)
+            return Form(Select(
+                *[Option(x.name, value=x.id, selected=(True if x.id == int(deg) else False)) for x in degreelist],
+                name="deg", id="deg", hx_post="/view/timetable/", hx_trigger="change"
+            ))
 
         course_list = [x.name for x in courses()]
-        print(course_list)
-        heading = f"Timetable | {year} | Semester {sem} | {degree}"
+        # print(course_list)
+        heading = H1(f"Timetable", get_year_dropdown(session['year']), get_sem_dropdown(session['sem']), get_degree_dropdown(deg), cls="grid")
         # table1 = Table(
         #     Tr(Th('Time'), Th('Monday'), Th("Tuesday"), Th("Wednesday"), Th("Thursday"),
         #        Th("Friday"), Th("Saturday"), Th("Sunday")),
@@ -21,8 +88,8 @@ def register_timetable(app, rt, db):
         #
         # )
 
-        time_list = [x.time for x in timetable(where="year='2024' and sem='3' and degree_id=1")]
-        ttble = timetable(where="year='2024' and sem='3' and degree_id=1")
+        time_list = [x.time for x in timetable(where=f"year='{year}' and sem='{sem}' and degree_id={int(deg)}")]
+        ttble = timetable(where=f"year='{year}' and sem='{sem}' and degree_id={int(deg)}")
 
         table2 = Table(
             Tr(Th('Day'), *([Th(x) for x in time_list])),
@@ -34,7 +101,7 @@ def register_timetable(app, rt, db):
             Tr(Td('Thursday'), *[Td(course_list[x.thu]) for x in ttble]),
             Tr(Td('Friday'), *[Td(course_list[x.fri]) for x in ttble]),
             Tr(Td('Saturday'), *[Td(course_list[x.sat]) for x in ttble]),
-            Tr(Td(A("Edit", href="/view/timetable/edit"), colspan=10)),
+            Tr(Td(A("Edit", href="/view/timetable/edit/"), colspan=10)),
             id="student_display", name="student_display",
 
         )
@@ -53,8 +120,24 @@ def register_timetable(app, rt, db):
         #     id="student_display", name="student_display",
         # )
 
-        page = Titled(heading, table2,Br(), style='margin:auto; width:70%;')
+        page = Div(heading, table2,Br(), style='margin:auto; width:70%;')
         return get_navbar(), page
+
+    @rt('/view/timetable/')
+    def post(session, year:str=None, sem:str=None, deg:str=None):
+        print("view",year, sem, deg)
+        if year : session['year'] = year
+        else : year = session['year']
+        if sem : session['sem'] = sem
+        else : sem = session['sem']
+        if deg : session['degree'] = deg
+        else : deg = session['degree']
+        print("view2", year, sem, deg)
+        print("Inside post", session)
+        # session['year'] = year if year else year = session['year']
+        # session['sem'] = sem if sem else sem = session['sem']
+        # session['degree'] = degree if degree else degree = session['degree']
+        return Meta(http_equiv="refresh", content=f"0;/view/timetable/?year={year}&sem={sem}&degree={deg}")
 
     def get_dropdown(course_list, current_value, day, time, rowid) :
 
@@ -93,11 +176,19 @@ def register_timetable(app, rt, db):
                              id=f"edit_main-{i}{x["id"]}")
         return cont
 
-    @rt('/view/timetable/edit')
-    def get(year:str="2024", sem:str="3", degree:str="Msc MI"):
+    @rt('/view/timetable/edit/')
+    def get(session, year:str=None, sem:str=None, deg:str=None):
+
+        if year : session['year'] = year
+        else : year = session['year']
+        if sem : session['sem'] = sem
+        else : sem = session['sem']
+        if deg : session['degree'] = deg
+        else : deg = session['degree']
+
         course_list = [x.name for x in courses()]
-        time_list = [x.time for x in timetable(where="year='2024' and sem='3' and degree_id=1")]
-        ttble = timetable(where="year='2024' and sem='3' and degree_id=1", as_cls=False)
+        time_list = [x.time for x in timetable(where=f"year='{year}' and sem='{sem}' and degree_id={int(deg)}")]
+        ttble = timetable(where=f"year='{year}' and sem='{sem}' and degree_id={int(deg)}", as_cls=False)
 
         collist = [x.name for x in timetable.columns]
         y = 5  # starting index of days in timetable table
@@ -133,4 +224,4 @@ def register_timetable(app, rt, db):
         # row = timetable.get(where)
         # collist = [x.name for x in timetable.columns]
         timetable.update(id=t["id"], updates={list(t.keys())[0]:list(t.values())[0]})
-        return Meta(http_equiv="refresh", content=f"0;/view/timetable/edit")
+        return Meta(http_equiv="refresh", content=f"0;/view/timetable/edit/")
